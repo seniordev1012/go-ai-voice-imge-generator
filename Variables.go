27beaaa9a62248a7ae925de69ab32d2b1bb04c53
seniordev1012 <zhurbadenys65@gmail.com>
2023-04-16 23:28:27 +0200

    #ADD- Vars

---
+++ Variables.go
@@ -0,0 +1,12 @@
+package main
+
+// This is where all your variables are stored for the program to use
+// Be careful when editing this file, as it can cause the program to not work
+// as expected
+// DON'T LEAK YOUR KEYS, THEY ARE USED TO ACCESS YOUR DATABASES AND OTHER SERVICES
+// I REPEAT, DO NOT LEAK YOUR KEYS.
+// NOW REPEAT WITH ME, "I WILL NOT LEAK MY TOKENS"!
+const (
+	openKeys   = "OPENAI"
+	speechKeys = "AZURE"
+)
